a79bab26eb63bd13fcd55f59dc466dbe86cbdd43
apsanregmi <mahesh.regmi@cloudproai.com>
2024-10-10 01:02:43 +0545

    menu double

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
 
         <div>
-      {/* <Navbar /> */}
+      <Navbar />
       {children}</div>
       </body>
     </html>
